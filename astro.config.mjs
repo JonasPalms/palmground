97e2575db46cf320e9245302b07d83db8ecf8b53
JonasPalms <jonas.p.sorensen@gmail.com>
2024-12-26 13:23:35 +0100

    convert demoes to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     svg: true,
   },
+
+  markdown: {
+    shikiConfig: {
+      theme: 'night-owl',
+    },
+  },
+
+  integrations: [mdx()],
 })
